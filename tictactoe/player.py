a1476df2b1042971aa8f016160e3d4bb388026fb
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-09-13 19:38:41 +0100

    Add files via upload

--- tictactoe/player.py
+++ tictactoe/player.py
@@ -19,7 +19,8 @@ class RandomComputerPlayer(Player):
 
 
     def get_move(self, game):
-        pass
+        square = random.choice(game.avalibale_moves())
+        return square
 
 class HumanPlayer(Player):
     def __init__(self, game):
@@ -27,3 +28,11 @@ class HumanPlayer(Player):
 
 
     def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move(0-9):')
+            ''' check correct value and if the spot uses wants is taken or
+            avaliable
+
+            '''
