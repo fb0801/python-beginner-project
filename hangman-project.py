c64252672148ad52f2e59bab1e5360338e611000
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-09-13 15:17:06 +0100

    Add files via upload

--- hangman-project.py
+++ hangman-project.py
@@ -4,30 +4,29 @@ from words import words
 
 def get_valid_word(words):
     word = random.choice(words)#randomly choose from the list
-    while '-' in word orr ' ' in word:
+    while '-' in word or ' ' in word:
         word = random.choice(words)
 
         return word
 
 def hangman():
-    word=get_valid_word(words):
-        word_letters = set(word)#letters in the word
-        alphabet = set(string.ascii_uppercase)
-        used_letters= set() #what user has guessed
+    word=get_valid_word(words)
+    word_letters = set(word)#letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters= set() #what user has guessed
 
-        #get user input
-        while len(word_letters) > 0:
-            #letters used
-            #' '.join(['a', 'b', 'cd']) --> 'a b cd'
-            print(' you have used these letters: ', ' '.join(used_letters))
-
-            
-            #current word is
-            word_list = [letter if letter in used_letters else '-' for letter in word]
-
-
+    #get user input
+    while len(word_letters) > 0:
+        #letters used
+        #' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print(' you have used these letters: ', ' '.join(used_letters))
 
             
+        #current word is
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print("Current word: ", ' '. join(word_list))
+
+
         user_letter = input('Guess a letter: ').upper()
         if  user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
