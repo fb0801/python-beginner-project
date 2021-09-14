8fb29d01b412a4ac7be69ff479649a4016139d14
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-09-14 19:38:14 +0100

    Add files via upload

--- minesweeper.py
+++ minesweeper.py
@@ -18,6 +18,29 @@ class Board:
 
     def make_new_board(self):
         #make a new board based on info given
+         board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
+        # this creates an array like this:
+        # [[None, None, ..., None],
+        #  [None, None, ..., None],
+        #  [...                  ],
+        #  [None, None, ..., None]]
+
+        # plant the bombs
+        bombs_planted = 0
+        while bombs_planted < self.num_bombs:
+            loc = random.randint(0, self.dim_size**2 - 1) # return a random integer N such that a <= N <= b
+            row = loc // self.dim_size  # we want the number of times dim_size goes into loc to tell us what row to look at
+            col = loc % self.dim_size  
+
+            if board[row][col] == '*':
+                # this means we've actually planted a bomb there already so keep going
+                continue
+
+            board[row][col] = '*' # plant the bomb
+            bombs_planted += 1
+
+        return board
+
 
 
     def assign_values_to_board(self):
@@ -28,30 +51,45 @@ class Board:
                 self.board[r][c] = self.get_num_neighboring_bombs(r,c)
 
     def get_num_neighboring_bombs(self, row, col):
-        for r in range(row-1, (row+1)+1):
-            for c in range(col-1, (col+1)+1):
-                
+        num_neighboring_bombs = 0
+        for r in range(max(0, row-1), min(self.dim_size-1,row+1)+1):
+            for c in range(max(0,col-1), min(self.dim_size - 1, col+1)+1):
+                if r == row and c == col:
+                    continue
+                if self.board[r][c] == '*':
+                    num_neightboring_bombs +=1
+
+        return num_neighboring_bombs
         
 
+    def dig(self, row, col):
+        #dig at the location
+        #hit bomb means end of game
 
-        #plant bomb
-        bombs_planted = 0
-        while bombs_planted < self.num_bombs:
-            loc = random.randint(0, self.dim_size**2 - 1)
-            row = loc // self.dim_size
-            col = loc % self.dim_size
+        self.dug.add((row, col))
 
-            if board[row][col] == '*':
-                #we have placed a bomb
-                continue
-            board[row][col]
-            bombs_planted +=1
+        if self.board[row][col] == '*':
+            return False
+        elif self.board[row][col] > 0:
+            return True
 
-        return board
+        for r in range(max(0, row-1), min(self.dim_size-1,row+1)+1):
+            for c in range(max(0,col-1), min(self.dim_size - 1, col+1)+1):
+                if (r,c) in self.dug:
+                    continue #dont dig where you already have
+                self.dig(r,c)
+        return True
+
+    def __str__(self):
+        #print the board for the player
+        visible_board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
+        for col in range(self.dim_size):
+            if (row, )
         
 
 def play(dim_size=10, num_bombs=10):
     #create and plant bomb
+    board = Board(dim_size, num_bombs)
     #show user the board and ask wher to dig next
     # if dig bomb gameover
     # if not a bomb continue
