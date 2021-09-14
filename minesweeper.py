beda73a16e683a3387bb779f7a4d344e3b31ca2c
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-09-14 18:24:05 +0100

    Add files via upload

--- minesweeper.py
+++ minesweeper.py
@@ -12,11 +12,15 @@ class Board:
 
 
         self.board = self.make_new_board()
+        self.assign_values_to_board()
 
         self.dug = set()
 
     def make_new_board(self):
         #make a new board based on info given
+
+
+    def assign_values_to_board(self):
 
 
         #make new board
@@ -26,7 +30,17 @@ class Board:
         #plant bomb
         bombs_planted = 0
         while bombs_planted < self.num_bombs:
-            loc = random.
+            loc = random.randint(0, self.dim_size**2 - 1)
+            row = loc // self.dim_size
+            col = loc % self.dim_size
+
+            if board[row][col] == '*':
+                #we have placed a bomb
+                continue
+            board[row][col]
+            bombs_planted +=1
+
+        return board
         
 
 def play(dim_size=10, num_bombs=10):
