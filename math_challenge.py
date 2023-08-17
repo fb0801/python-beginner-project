400bfdad3b1e11161831df5c763d4b722fc6fc6a
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-08-17 15:36:10 +0100

    Add files via upload

--- math_challenge.py
+++ math_challenge.py
@@ -1,16 +1,19 @@
 import random
+import time
 
 OPERATORS = ['+', '-', '/', '*']
 MIN_OPERAND = 3
 MAX_OPERAND = 12
+TOTAL_PROBLEMS = 10
 
 def generate_problem():
     left = random.randint(MIN_OPERAND, MAX_OPERAND)
     right = random.randint(MIN_OPERAND, MAX_OPERAND)
     operator = random.choice(OPERATORS)
 
-    expr = str(left)+ ' ' + right + " " +str(operator)
-    print(expr)
-    return expr
+    expr = str(left)+ ' ' + operator + " " +str(right)
+    answer = eval(expr)
+    return expr, answer
 
-generate_problem()
+expr, answer = generate_problem()
+print(expr, answer)
