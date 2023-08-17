0a81321fbc1af7a9b5d847c4c66049529fa19db4
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-08-17 15:49:10 +0100

    Add files via upload

--- math_challenge.py
+++ math_challenge.py
@@ -5,6 +5,7 @@ OPERATORS = ['+', '-', '/', '*']
 MIN_OPERAND = 3
 MAX_OPERAND = 12
 TOTAL_PROBLEMS = 10
+wrong = 0
 
 def generate_problem():
     left = random.randint(MIN_OPERAND, MAX_OPERAND)
@@ -15,5 +16,21 @@ def generate_problem():
     answer = eval(expr)
     return expr, answer
 
-expr, answer = generate_problem()
-print(expr, answer)
+input('Press enter to start!')
+print('-----------------')
+
+start_time = time.time()
+
+for i in range(TOTAL_PROBLEMS):
+    expr, answer = generate_problem()
+    while True:
+        guess = input('Problem #' + str(i + 1) + ': ' + expr + '= ')
+        if guess == str(answer):
+            break
+        wrong +=1
+
+end_time = time.time()
+total_time = round(end_time - start_time, 2)
+
+print('-----------------')
+print('Nice work! You finished in', total_time, 'seconds!')
